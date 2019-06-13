c4f972d210c1a8ba430dda3d85a49adb75b0d129
ASHOK MAHARJAN <ashok997@gmail.com>
2019-06-13 03:03:50 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require 'pry'
 
 class EmailParser
@@ -23,5 +24,29 @@ class EmailParser
     list.flatten.uniq
     ##list.uniq
     
+=======
+class EmailParser
+  attr_reader :email_list
+  
+  @@all = []
+  
+  def initialize (email_list)
+    
+    @@email_list = email_list.split(",")
+    ##self
+    ##@@all << self
+  end
+ 
+ def self.new(emails)
+  
+    email = emails.split(",")
+    parser = self
+    parser.email_list=email
+    parser
+  end
+  
+  def parse
+    @@all.find_all{|i| i }
+>>>>>>> 477a69d1e14bb202005e84c7fc587cce071a3303
   end
 end
